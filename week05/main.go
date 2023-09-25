8b20d405e2a027e91d48909bb98d30d006080fd1
ss526611 <ss526611@gamil.com>
2023-09-25 17:32:41 +0900

    week05 v0.8

--- week05/main.go
+++ week05/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
 func main() {
-	HotSpurs := "hm ? j mai?"
+	fmt.Print("Input score : ")
+	reader := bufio.NewReader(os.Stdin)
+	inputScore := reader.ReadString('\n') // 1 variable but reader.ReadString returns 2 values
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(inputScore)
+	if inputScore >= 90 {
+		grade := "A grade!"
+	} else {
+		grade := "under A grade..."
+	}
 }
