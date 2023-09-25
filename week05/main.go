59558a15c91e1bb304e66c4114a7a08c757b22a9
ss526611 <ss526611@gamil.com>
2023-09-25 17:09:48 +0900

    week05 v0.3

--- week05/main.go
+++ week05/main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 func main() {
-	var now time.Time = time.Now()
-	now = time.Now()
-	// var year int  = now.Year()
-	var year = now.Year()
-	month := now.Month()
-	fmt.Println(year, month)
+	HotSpurs := "hm ? j mai?"
 }
