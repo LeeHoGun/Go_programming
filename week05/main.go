abb4ea93652536a7b3978f3ae5f2d0d57ee9f291
ss526611 <ss526611@gamil.com>
2023-09-25 17:55:21 +0900

    week0t v1.1

--- week05/main.go
+++ week05/main.go
@@ -1,31 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+	"math/rand"
 )
 
 func main() {
-	fmt.Print("Input score : ")
-	reader := bufio.NewReader(os.Stdin)
-	inputScore := reader.ReadString('\n') // 1 variable but reader.ReadString returns 2 values
-	if err != nil {
-		log.Fatal(err)
-	}
-	inputScoreString = strings.TrimSpace(inputScoreString)
-	inputScore, err := strconv.ParseFloat(inputScoreString, 32)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var grade string
-	if inputScore >= 90 {
-		grade = "A grade!"
-	} else {
-		grade = "under A grade..."
-	}
-	fmt.Println("You got", grade)
+	dice := rand.Intn(6) + 1
+	fmt.Println(dice)
 }
