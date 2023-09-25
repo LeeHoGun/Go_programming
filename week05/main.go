20b6ffdad0b2c0e4cdddaf085c06149489a2f26f
ss526611 <ss526611@gamil.com>
2023-09-25 18:15:34 +0900

    week05 v1.1

--- week05/main.go
+++ week05/main.go
@@ -1,11 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
-	fmt.Print("Guess number game!")
-	answer := rand.intn(100) + 1 // 1 ~ 100
+	rand.Seed(time.Now().Unix()) // get the current data and time as an integer
+	answer := rand.Intn(100) + 1 // random integer number (1 ~ 100)
+	fmt.Println("Guess Number Game~")
 	fmt.Println(answer)
+	reader := bufio.NewReader(os.Stdin)
+	for i := 0; i < 10; i++ {
+		fmt.Println("You have", 10-i, "chances~")
+		fmt.Print("Input guess number : ")
+		inputGuessString, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		inputGuessString = strings.TrimSpace(inputGuessString)
+		inputGuess, err := strconv.Atoi(inputGuessString)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if inputGuess < answer {
+			if inputGuess == answer {
+				fmt.Println("Great! U got number. congratulations~") // Answer is higher~
+				break
+			} else if inputGuess < answer {
+				fmt.Println("Guess number is lower then answer") // Answer is higher~
+			} else if inputGuess > answer {
+				fmt.Println("Guess number is higher then answer") // Answer is lower
+			}
+		}
+	}
 }
