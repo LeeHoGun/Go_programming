0c4d3923eb12c09321c2e32c6c0332deb6a00c0d
ss526611 <ss526611@gamil.com>
2023-10-23 17:40:33 +0900

    week09 v1

--- week09/main.go
+++ week09/main.go
@@ -2,11 +2,15 @@ package main
 
 import "fmt"
 
-func double(n *int) {
-	*n = *n * 2
+func double(n1 *int, n2 *int) { // 두 수를 교환
+	temp := *n1
+	*n1 = *n2
+	*n2 = temp
 }
 func main() {
-	var a int = 5
-	double(&a) // pass by pointer
-	fmt.Printf("%d\n", a)
+	a := 10
+	b := 20
+
+	c := &a // 정수형 타입의 pointer
+	fmt.Printf("%d %d %d\n", a, b, c)
 }
