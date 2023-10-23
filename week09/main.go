21133b16e1cbb8354337f6dd492ad8289556da70
ss526611 <ss526611@gamil.com>
2023-10-23 17:44:12 +0900

    week09 v2

--- week09/main.go
+++ week09/main.go
@@ -8,9 +8,9 @@ func double(n1 *int, n2 *int) { // 두 수를 교환
 	*n2 = temp
 }
 func main() {
-	a := 10
-	b := 20
-
-	c := &a // 정수형 타입의 pointer
-	fmt.Printf("%d %d %d\n", a, b, c)
+	a := 1
+	b := 2
+	c := &a // 정수형 타입의 pointer | var c *int = &a
+	// fmt.Printf("%d %d %d\n", a, b, c)
+	fmt.Printf("%d %d %d\n", a, b, *c)
 }
