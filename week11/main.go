55c7ac0254c7f8ebcc74672c8b8d07a716707a08
ss526611 <ss526611@gamil.com>
2023-11-06 17:15:25 +0900

    week11 v2.4

--- week11/main.go
+++ week11/main.go
@@ -15,4 +15,16 @@ func main() {
 	primes := [3]int{2, 3, 5} // 배열 리터럴로 primes 배열 초기화
 	fmt.Println(primes, primes[1])
 
+	test := [5]bool{true, true, true} // 초기화 하지 않은 원소의 제로 값은 해당 원소의 타입의
+	fmt.Println(test[3])
+
+	// for i := 0; i < 3; i++ {
+	// 	fmt.Println(primes[i])
+	// }
+
+	i := 0
+	for i < 4 { //panic: runtime error: index out of range [3] with length 3
+		fmt.Println(primes[i])
+		i++
+	}
 }
