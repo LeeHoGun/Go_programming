c315e8e50b933375010ac81d8891d8d32faf9891
ss526611 <ss526611@gamil.com>
2023-11-13 17:15:22 +0900

    week12 v3.0

--- week12/main.go
+++ week12/main.go
@@ -19,8 +19,14 @@ func main() {
 	}
 
 	test := [3]string{"inha", "go", "student"}
-	testS := test[:2] // testS := test[0:2]
+	testS := test[:2]  // testS := test[0:2]
+	testS2 := test[1:] // index 1부터 끝까지
+
+	testS2[0] = "python"
+	// test[1] = "python"
+	// testS[-2] = "python"
+
 	fmt.Println(test, len(test))
 	fmt.Println(testS, len(testS))
-
+	fmt.Println(testS2, len(testS2))
 }
